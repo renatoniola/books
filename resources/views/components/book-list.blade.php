e0aa140a5ad86fb8715209074ee2980b988ab584
Renato Niola <niolarenato01@gmail.com>
2024-05-05 14:30:31 +0200

    changed n/a to ---

--- resources/views/components/book-list.blade.php
+++ resources/views/components/book-list.blade.php
@@ -10,7 +10,7 @@
     </div>
     <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
 
-      <p class="text-sm leading-6 text-gray-900">Published: {{ $book->book_year_published > 0 ? $book->book_year_published : 'N/A' }}</p>
+      <p class="text-sm leading-6 text-gray-900">Published: {{ $book->book_year_published > 0 ? $book->book_year_published : '----' }}</p>
       <p class="mt-1 text-xs leading-5 text-gray-500">
         @if (!Auth::guest()) 
         @livewire('status-button',
