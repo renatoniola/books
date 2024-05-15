acdd6016eead73ab52dda3df5f52d129b049c9b6
Renato Niola <niolarenato01@gmail.com>
2024-05-15 22:03:50 +0200

    changes to build

--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\models\Author;
+use App\Models\Author;
 
 class AuthorSeeder extends Seeder
 {
--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\models\Book;
+use App\Models\Book;
 
 class BookSeeder extends Seeder
 {
