a7954412b6913dd2a9a5ef3141343fe09ae15855
Renato Niola <niolarenato01@gmail.com>
2024-08-29 00:27:38 +0200

    removed old logo

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -108,7 +108,7 @@
       <div class="flex items-center justify-between">
         <a href="#" class="flex -m-1.5 p-1.5">
           <span class="sr-only mr-8">Books</span>
-          <img class="h-8 w-8" src="{{ Vite::image('books.png') }}">Books
+          <img class="h-8 w-8" src="{{ Vite::image('logo.png') }}">Books
         </a>
         <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
           <span class="sr-only">Close menu</span>
